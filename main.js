8765875b7f4dac97f432d15faf5902d8661b0c57
Balabrucks05 <balasubramani@sparkouttech.com>
2024-10-09 10:59:20 +0530

    route structures

--- main.js
+++ main.js
@@ -7,6 +7,22 @@ app.get("/",(req,res) => {
     res.json({Msg: "This is my first commit!" });
 });
 
+//CRUD functionality movies
+
+// R - For Reading movies
+app.get("/movies", () => {});
+
+// C - For Creating movies
+app.post("/movies", () => {});
+
+// U - For Updating movies
+app.put("/movies/:id", () => {});
+
+//D - For Deleting movies
+app.delete("/movies/:id", () => {});
+
+
+
 app.listen(port,()=>{
     console.log(`This server is running at http://localhost:${port}`);
     
